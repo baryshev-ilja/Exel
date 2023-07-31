9cee00804cf6499ebef38c6087ef1dc5fc81359b
Baryshewww <baryshev-tm@mail.ru>
2023-07-31 14:25:36 +0300

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['google'],
+  plugins: ['babel', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'object-curly-spacing': 'off',
+    'require-jsdoc': 'off',
+  },
+};
